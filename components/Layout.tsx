328cad37ae2eaeb4ab8b74525521be75671f8420
WindzLord <ii.khackien@gmail.com>
2023-03-27 15:53:50 +0700

    Test seo

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,23 +1,33 @@
-import React, { ReactNode } from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
+import React, { ReactNode } from "react";
+import Link from "next/link";
+import Head from "next/head";
 
 type Props = {
-  children?: ReactNode
-  title?: string
-}
+  children?: ReactNode;
+  title?: string;
+  description?: string;
+};
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
+const Layout = ({
+  children,
+  title = "This is the default title",
+  description,
+}: Props) => (
   <div>
     <Head>
-      <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+      <title>{title}</title>
+      <meta name="description" content={description} />
+
+      <meta name="og:title" content={title} />
+      <meta name="og:description" content={description} />
     </Head>
     <header>
       <nav>
-        <Link href="/">Home</Link> | <Link href="/about">About</Link> |{' '}
-        <Link href="/users">Users List</Link> |{' '}
+        <Link href="/">Home</Link> | <Link href="/about">About</Link> |{" "}
+        <Link href="/users">Users List</Link> |{" "}
         <a href="/api/users">Users API</a>
       </nav>
     </header>
@@ -27,6 +37,7 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <span>I'm here to stay (Footer)</span>
     </footer>
   </div>
-)
+);
 
-export default Layout
+export default Layout;
+
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,14 +1,24 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from "next/link";
+import { useRouter } from "next/router";
+import Layout from "../components/Layout";
 
-const AboutPage = () => (
-  <Layout title="About | Next.js + TypeScript Example">
-    <h1>About</h1>
-    <p>This is the about page</p>
-    <p>
-      <Link href="/">Go home</Link>
-    </p>
-  </Layout>
-)
+const AboutPage = () => {
+  const router = useRouter();
+  const { user, reward = "100 AGI" } = router.query;
 
-export default AboutPage
+  return (
+    <Layout
+      title="About | Next.js + TypeScript Example"
+      description={`Hello AboutPage` + (user ? ` | ${user}: ${reward}` : ``)}
+    >
+      <h1>About</h1>
+      <p>This is the about page</p>
+      <p>
+        <Link href="/">Go home</Link>
+      </p>
+    </Layout>
+  );
+};
+
+export default AboutPage;
+
