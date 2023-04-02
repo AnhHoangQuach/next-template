43b12a9321fa3f8bab961a8d13dea86b2625700e
WindzLord <ii.khackien@gmail.com>
2023-04-02 22:21:39 +0700

    MInor

--- src/containers/AppFooter.tsx
+++ src/containers/AppFooter.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Container, IconButton, Toolbar } from '@mui/material';
+import { AppBar, Container, IconButton, Toolbar, Tooltip } from '@mui/material';
 
 const AppFooter = () => {
   return (
@@ -7,22 +7,27 @@ const AppFooter = () => {
         <div className='flex items-center gap-2 ml-auto'>
           {[
             {
+              name: 'Discord',
               icon: 'fa-brands fa-discord',
               url: 'https://discord.gg/xGuKne5Mye',
             },
             {
+              name: 'Twitter',
               icon: 'fa-brands fa-twitter',
               url: 'https://twitter.com/AuragiFinance',
             },
             {
+              name: 'GitBook',
               icon: 'fa-solid fa-file-lines',
               url: 'https://docs.auragi.finance/auriga-finance',
             },
           ].map((item, index) => (
-            <a key={index} href={item.url} target='_blank'>
-              <IconButton sx={{ width: 52, height: 52 }}>
-                <i className={item.icon + ' text-3xl text-primary-light'} />
-              </IconButton>
+            <a key={index} href={item.url} target='_blank' className='rounded-full'>
+              <Tooltip title={item.name}>
+                <IconButton sx={{ width: 52, height: 52 }}>
+                  <i className={item.icon + ' text-3xl text-primary-light'} />
+                </IconButton>
+              </Tooltip>
             </a>
           ))}
         </div>
